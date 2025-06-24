6552bc97895e2db837790c91b5019c633619e641
testeryup <enzo.edwin.zzz@gmail.com>
2025-06-25 00:52:19 +0700

    add build assets for prod

---
+++ public/build/assets/input-error-VKjxW5Xi.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BzdOADVP.js";import{a as o}from"./app-logo-icon-BkO5Ozxv.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
